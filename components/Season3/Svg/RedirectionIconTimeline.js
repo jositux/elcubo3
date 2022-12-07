5f44bec5a4e06b574f4ca357d498b8fc6f916fa8
nicolas iriarte <nidaniel1291@gmail.com>
2022-12-07 11:52:04 -0300

    timeline1 desktop,mobile

---
+++ components/Season3/Svg/RedirectionIconTimeline.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const RedirectionIconTimeline = () => {
+  return (
+    <svg 
+      width="38" 
+      height="38" 
+      viewBox="0 0 38 38" 
+      fill="none" 
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_829_1024)">
+        <path d="M17.4163 11.084H9.49967C8.65982 11.084 7.85437 11.4176 7.2605 12.0115C6.66664 12.6053 6.33301 13.4108 6.33301 14.2507V28.5007C6.33301 29.3405 6.66664 30.146 7.2605 30.7398C7.85437 31.3337 8.65982 31.6673 9.49967 31.6673H23.7497C24.5895 31.6673 25.395 31.3337 25.9888 30.7398C26.5827 30.146 26.9163 29.3405 26.9163 28.5007V20.584" stroke="#3B2807" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M15.833 22.1673L31.6663 6.33398" stroke="#3B2807" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M23.75 6.33301H31.6667V14.2497" stroke="#3B2807" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      </g>
+      <defs>
+        <clipPath id="clip0_829_1024">
+          <rect width="38" height="38" fill="white"/>
+        </clipPath>
+      </defs>
+    </svg>
+
+  )
+}
+
+export default RedirectionIconTimeline
