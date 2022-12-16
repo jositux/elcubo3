d0504ab0cb3f3b112e42272601807cb8eedad1c3
josi <josi@interfaz.co>
2022-12-16 14:27:23 -0300

    Update changes

--- components/Season3/Svg/PlayIconTimeline.js
+++ components/Season3/Svg/PlayIconTimeline.js
@@ -7,7 +7,6 @@ const PlayIconTimeline = () => {
     height={30}
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    {...props}
   >
     <g
       clipPath="url(#a)"
--- components/Season3/Svg/RedirectionIconTimeline.js
+++ components/Season3/Svg/RedirectionIconTimeline.js
@@ -7,7 +7,6 @@ const RedirectionIconTimeline = () => {
     height={30}
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    {...props}
   >
     <g
       clipPath="url(#a)"
