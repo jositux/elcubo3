6130898da44732f8de8d4e802a2b132b79e22d01
josi <josi@interfaz.co>
2023-01-26 16:07:40 -0300

    Update json

--- pages/el-cubo/temporada-3/interactivo/[personaje].tsx
+++ pages/el-cubo/temporada-3/interactivo/[personaje].tsx
@@ -317,7 +317,7 @@ const Personaje = (props) => {
 export const getServerSideProps = async (context) => {
   const { params } = context;
   const { personaje } = params;
-  const dataRaw = await rawFetch('https://jositux.com/json/all.json');
+  const dataRaw = await rawFetch('https://elcubo3.vercel.app/js/all.json');
   const dataJson = await dataRaw.json();
   const filtteredData = dataJson.filter(d => personaje === d.field_ec_title);
 
