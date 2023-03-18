89358a52bbf44fd14be859a481d8deaed4218b99
josi <jm.guaimas@gmail.com>
2023-03-18 00:15:13 -0300

    Update Links

--- components/Labyrinth/BackToCharacters.tsx
+++ components/Labyrinth/BackToCharacters.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const BackToCharacters = ({text}) => {
+const BackToCharacters = ({ text }) => {
   return (
     <div className="header-top">
       <div className="header-top-inner">
         <nav className="nav">
-          <a href="/el-cubo/temporada-1/personajes" className="back-to-season">
+          <a href="/" className="back-to-season">
             <img src="/images/icon-arrow-back.svg" />
             <span>{text}</span>
           </a>
