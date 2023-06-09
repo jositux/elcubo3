433848caf4a8b894ce26e3179d307b1401ce021b
josi <jm.guaimas@gmail.com>
2023-06-09 17:41:06 -0300

    Update Sliders2.tsx

--- components/Season3/Season/Desktop/Sliders2.tsx
+++ components/Season3/Season/Desktop/Sliders2.tsx
@@ -1,6 +1,7 @@
 import React, { Fragment, useEffect, useRef, useState } from 'react';
 import ButtonHome from 'components/Season3/Shared/Button/ButtonHome';
 import Credits from 'components/Season3/Season/Credits/Credits';
+import HtmlParser from 'html-react-parser';
 
 import styles from './season3.slider2.desktop.module.scss';
 import { useRouter } from 'next/router';
@@ -9,12 +10,6 @@ import { useRouter } from 'next/router';
 export const SlidersDesktop2 = ({ isLoggedIn, onGuest }) => {
 
   const router = useRouter();
-
-  let phrase_1 =
-    router.locale === "en"
-      ? "Welcome to Jordan Sube, a town with people who resists  and knows how to live"
-      : "Bienvenido y bienvenida a Jordán Sube, un pueblo con gente que resiste y sabe vivir.";
-
 
   useEffect(() => {
 
@@ -181,7 +176,13 @@ export const SlidersDesktop2 = ({ isLoggedIn, onGuest }) => {
           <img id='ImgBG0' src="/images/season3/slider/0.jpg" className={styles.ImgBG0} />
           <div id='Texto0' className={`${styles.TextContent} ${styles.TextContentRight}`}>
             <article>
-              <h1>{phrase_1}</h1>
+              <h1>
+                {
+                  router.locale === "en" ?
+                    "Welcome to Jordan Sube, a town with people who resists  and knows how to live" :
+                    "Bienvenido y bienvenida a Jordán Sube, un pueblo con gente que resiste y sabe vivir."
+                }
+              </h1>
             </article>
           </div>
         </div>
@@ -190,8 +191,18 @@ export const SlidersDesktop2 = ({ isLoggedIn, onGuest }) => {
           <img id='ImgBG1' src="/images/season3/slider/1.jpg" className={styles.ImgBG1} />
           <div id='Texto1' className={`${styles.TextContent} ${styles.TextContentLeft}`}>
             <article>
-              <h1>Jordán fue próspero, pero ahora convive en una frágil realidad.</h1>
-              <p>A lo largo de su historia, ha estado sumido en la violencia, la pobreza y el extenso mandato de una sola familia. Hoy quedan poco más de 1.000 habitantes.</p>
+              <h1>
+                {
+                  router.locale === "en" ?
+                    "Jordan was prosperous, but now the town lives with a fragile reality." :
+                    "Jordán fue próspero, pero ahora convive en una frágil realidad."
+                }
+              </h1>
+              <p>{
+                router.locale === "en" ?
+                  "Throughout its history, it has been mired in violence, poverty and under the power of a single family. Today, less than 1000 inhabitants are left." :
+                  "A lo largo de su historia, ha estado sumido en la violencia, la pobreza y el extenso mandato de una sola familia. Hoy quedan poco más de 1.000 habitantes."
+              }</p>
             </article>
           </div>
         </div>
@@ -200,8 +211,22 @@ export const SlidersDesktop2 = ({ isLoggedIn, onGuest }) => {
           <img id='ImgBG2' src="/images/season3/slider/2.jpg" className={styles.ImgBG2} />
           <div id='Texto2' className={`${styles.TextContent} ${styles.TextContentRight}`}>
             <article >
-              <h1>Viaja con nosotros <br />a este lugar. </h1>
-              <p>Recorre los Caminos de Jordán de la mano de personajes entrañables, explora sus historias en el orden y al ritmo que quieras, y completa la foto del pueblo con información que será revelada al final de esta travesía. </p>
+              <h1>
+                {
+                  router.locale === "en" ?
+                    HtmlParser("Travel with us<br /> to this place") :
+                    HtmlParser("Viaja con nosotros <br /> a este lugar")
+                }
+              </h1>
+
+
+              <p>
+                {
+                  router.locale === "en" ?
+                    "Walk through Jordan roads accompanied by dear characters. Explore its stories in the order you want and at your own pace. Finally, complete the picture of the town with information that will be revealed only at the end of this journey." :
+                    "Recorre los Caminos de Jordán de la mano de personajes entrañables, explora sus historias en el orden y al ritmo que quieras, y completa la foto del pueblo con información que será revelada al final de esta travesía."
+                }
+              </p>
             </article>
           </div>
         </div>
@@ -212,10 +237,20 @@ export const SlidersDesktop2 = ({ isLoggedIn, onGuest }) => {
 
             <div className={styles.ButtonContainerContent}>
               <article>
-                <h2>Este viaje es una forma de entender<br />la propia realidad y recobrar la fuerza.</h2>
+                <h2>
+                  {
+                    router.locale === "en" ?
+                      HtmlParser("This journey is a way to understand<br /> our own reality and recover the strenght.") :
+                      HtmlParser("Este viaje es una forma de entender<br />la propia realidad y recobrar la fuerza.")
+                  }
+                </h2>
 
                 <div className={`${styles.buttonPulse}`}>
-                  <ButtonHome url={"/el-cubo/temporada-3/personajes?ref=init"} text="Iniciar EL Viaje" />
+                  <ButtonHome url={"/el-cubo/temporada-3/personajes?ref=init"} text={
+                    router.locale === "en" ?
+                      "Begin the journey" :
+                      "Iniciar el viaje"
+                  } />
                 </div>
               </article>
             </div>
